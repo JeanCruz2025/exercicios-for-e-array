90a6eeef85bf78537d9771c7d9e8d6cd1d4c482c
JeanCruz2025 <165338134+JeanCruz2025@users.noreply.github.com>
2024-05-02 20:05:06 -0300

    Add files via upload

---
+++ exercicio 1 for.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+ int numero ;
+ for (numero = 0;numero <= 10;numero ++) {
+    printf ("%d\n", numero);
+ }
+}
